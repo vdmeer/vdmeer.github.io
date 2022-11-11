95c6ad64a5449039e9346c27963d3ee75d8ca51e
Sven van der Meer <vdmeer.sven@mykolab.com>
2022-11-11 15:06:08 +0000

    Create mail.js
    first file for linktree

---
+++ linktree/mail.js
@@ -0,0 +1,9 @@
+function m_myk() {
+    var mm = document.getElementById("m_myk")
+    mm.innerHTML = "<i class='fa fa-envelope'></i>vdmeer.sven"
+}
+
+function m_gm() {
+    var mm = document.getElementById("m_gm")
+    mm.innerHTML = "<i class='fa fa-envelope'></i>vdmeer.sven"
+}
